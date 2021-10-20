1ccacc7f18950d2f6365f961a68437f1970216f9
DESKTOP-6D0RJD7\qwer <ritch1530@gmail.com>
2021-10-20 18:04:36 +0900

    base form

---
+++ src/components/TicTacToe.vue
@@ -0,0 +1,21 @@
+<template>
+  <table-component></table-component>
+</template>
+
+<script>
+import TableComponent from './TableComponent';
+export default {
+  components: {
+    TableComponent,
+  },
+  data() {
+    return {
+
+    };
+  },
+}
+</script>
+
+<style lang="">
+  
+</style>
